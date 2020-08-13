a16447da06473dc767e2dc5848bbc923e6cf91d4
Rafael Vieira <rafael.vieirasfair@gmail.com>
2020-08-12 22:10:48 -0300

    testa se cpf foi cadastrado

--- Vendas/Cliente.cs
+++ Vendas/Cliente.cs
@@ -6,9 +6,25 @@ namespace Vendas
 {
     class Cliente
     {
+        //construtor ctor
+        public Cliente(string nome, string cpf)
+        {
+            Nome = nome;
+            Cpf = cpf;
+        }
+        public Cliente()
+        {
+            criadoEm = DateTime.Now;
+        }
         //Atributos, propriedades e caracteristica
         public string Nome { get; set; }
         public string Cpf { get; set; }
-                        
+
+        public DateTime criadoEm {get; set; }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome} | CPF: {Cpf} | Criado em: {criadoEm}";
+        }
     }
 }
--- Vendas/Program.cs
+++ Vendas/Program.cs
@@ -3,89 +3,112 @@ using System.Collections.Generic;
 
 namespace Vendas
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
+    class Program
+    {
+        static void Main(string[] args)
+        {
 
-			int escolha;
-			Cliente c = new Cliente();
-			List<Cliente> clientes = new List<Cliente>();
+            int escolha;
+            Cliente c = new Cliente();
+            List<Cliente> clientes = new List<Cliente>();
 
-			do
-			{
-				// Opcoes do menu
-				Console.WriteLine("Escolha uma das Opcoes abaixo:\n");
-				Console.WriteLine("1 – Cadastrar de Cliente");
-				Console.WriteLine("2 – Listar de Clientes");
-				Console.WriteLine("3 – Cadastrar de Vendedor");
-				Console.WriteLine("4 – Listar de Vendedores");
-				Console.WriteLine("5 – Cadastrar de Produto");
-				Console.WriteLine("6 – Listar de Produtos");
-				Console.WriteLine("7 – Registrar Venda ");
-				Console.WriteLine("8 – Listar Vendas ");
-				Console.WriteLine("9 – Listar Vendas por Cliente");
-				Console.WriteLine("0 – Sair\n");
+            do
+            {
+                // Opcoes do menu
+                Console.WriteLine("Escolha uma das Opcoes abaixo:\n");
+                Console.WriteLine("1 – Cadastrar de Cliente");
+                Console.WriteLine("2 – Listar de Clientes");
+                Console.WriteLine("3 – Cadastrar de Vendedor");
+                Console.WriteLine("4 – Listar de Vendedores");
+                Console.WriteLine("5 – Cadastrar de Produto");
+                Console.WriteLine("6 – Listar de Produtos");
+                Console.WriteLine("7 – Registrar Venda ");
+                Console.WriteLine("8 – Listar Vendas ");
+                Console.WriteLine("9 – Listar Vendas por Cliente");
+                Console.WriteLine("0 – Sair\n");
 
-				// recebe a opcao escolhida
-				escolha = Convert.ToInt32(Console.ReadLine());
-			
-				switch (escolha)
-				{
-					
-					case 1:
-						
-						Console.WriteLine("-----LISTAR CLIENTES-----");
-						Console.WriteLine("Digite o Nome:");
-						c.Nome = Console.ReadLine();
-						Console.WriteLine("Digite o Cpf:");
-						c.Cpf = Console.ReadLine();
+                // recebe a opcao escolhida
+                escolha = Convert.ToInt32(Console.ReadLine());
 
-						clientes.Add(c);
+                switch (escolha)
+                {
 
-						//mensagem de sucesso
-						Console.WriteLine($"Cliente {c.Nome} de CPF: {c.Cpf} adicionado com sucesso.");
-						
-						break;
-					case 2:
-						// laco mostra todos os clientes
-						foreach (Cliente cli in clientes )
+                    case 1:
+                        c = new Cliente();
+                        Console.WriteLine("-----CADASTRAR CLIENTE-----");
+                        Console.WriteLine("Digite o Nome:");
+                        c.Nome = Console.ReadLine();
+                        Console.WriteLine("Digite o Cpf:");
+                        c.Cpf = Console.ReadLine();
+
+                        Boolean cpfCadastrado = false;
+                        //Valida se o cpf ja foi cadastrado
+                        foreach (Cliente cli in clientes)
                         {
-							Console.WriteLine($"Cliente {c.Nome} de CPF: {c.Cpf}\n");
-						}
+                            if (cli.Cpf == c.Cpf)
+                            {
+                                cpfCadastrado = true;
+                            }
+                            else
+                            {
+                                cpfCadastrado = false;
+                            }
+                        }
 
-					break;
-
-					case 3:
-
-					break;
-					case 4:
-
-					break;
-					case 5:
-
-					break;
-					case 6:
-
-					break;
-					case 7:
-
-					break;
-					case 8:
-
-					break;
-					case 9:
-
-					break;
-					
-					default:
-						if (escolha != 0)
+                        if (cpfCadastrado)
                         {
-							Console.WriteLine("Numero {0} Invalido\n", escolha);
-						}
-					break;
-				}
-			} while (escolha != 0); // botao para sair
-		}
-	}
+                            Console.WriteLine("Cpf ja cadastrado\n");
+                        }
+                        else
+                        {
+                            clientes.Add(c);
+                            //mensagem de sucesso
+                            Console.WriteLine($"Cliente {c.Nome} de CPF: {c.Cpf} adicionado com sucesso.");
+                        }
+
+                        
+
+                        break;
+                    case 2:
+                        // laco mostra todos os clientes
+                        Console.WriteLine("-----LISTAR CLIENTE-----");
+                        foreach (Cliente cli in clientes)
+                        {
+                            Console.WriteLine(cli);
+                        }
+
+                        break;
+
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+
+                        break;
+                    case 6:
+
+                        break;
+                    case 7:
+
+                        break;
+                    case 8:
+
+                        break;
+                    case 9:
+
+                        break;
+
+                    default:
+                        if (escolha != 0)
+                        {
+                            Console.WriteLine("Numero {0} Invalido\n", escolha);
+                        }
+                        break;
+                }
+            } while (escolha != 0); // botao para sair
+        }
+    }
 }
